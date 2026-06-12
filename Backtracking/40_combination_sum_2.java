8c3570487cf58240fb5952b8ba5c1cac540b3ece
Likhitha K <likhithak757@gmail.com>
2026-06-12 11:34:14 +0530

    combination sum 2 is added

---
+++ Backtracking/40_combination_sum_2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        Arrays.sort(candidates);
+        return subset(0, candidates, target, ans, ds);
+    }
+
+    public List<List<Integer>> subset(int ind, int[] num, int target, List<List<Integer>> ans, List<Integer> ds) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(ds));
+            return ans;
+        }
+        for (int i = ind; i < num.length; i++) {
+            if (i != ind && num[i] == num[i - 1])
+                continue;
+            if (num[i] > target)
+                break;
+            ds.add(num[i]);
+            subset(i + 1, num, target - num[i], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+        return ans;
+    }
+}
