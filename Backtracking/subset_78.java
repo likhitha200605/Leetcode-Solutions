6a9ed3ae38bb45cef271c4c4961d72c21adca782
Likhitha K <likhithak757@gmail.com>
2026-06-13 11:39:35 +0530

    subset leetcode number78 is added

---
+++ Backtracking/subset_78.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        recSubset(0, nums, ans, ds);
+        return ans;
+    }
+
+    public void recSubset(int index, int[] nums, List<List<Integer>> ans, List<Integer> ds) {
+        if (index == nums.length) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        recSubset(index + 1, nums, ans, ds);
+        ds.add(nums[index]);
+        recSubset(index + 1, nums, ans, ds);
+        ds.remove(ds.size() - 1);
+    }
+}
