617a683bc24b50d7aa7701091a53f0978bb11784
Likhitha K <likhithak757@gmail.com>
2026-06-13 11:40:40 +0530

    subset leetcode number78 is changed

--- Backtracking/subset_78.java
+++ Backtracking/subset_78.java
@@ -1,3 +1,8 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
