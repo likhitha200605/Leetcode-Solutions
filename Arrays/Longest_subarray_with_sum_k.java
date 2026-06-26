a615636d088802406125aadaf42978f2702a6486
Likhitha K <likhithak757@gmail.com>
2026-06-26 21:02:43 +0530

    longest subarray with sum k

--- Arrays/Longest_subarray_with_sum_k.java
+++ Arrays/Longest_subarray_with_sum_k.java
@@ -1,29 +1,31 @@
-public static int longestSubarray(int[] arr, int k) {
+class LongestSubarrayWithSumK {
+    public static int longestSubarray(int[] arr, int k) {
 
-    int left = 0, right = 0;
-    int sum = arr[0];
-    int maxLen = 0;
-    int n = arr.length;
+        int left = 0, right = 0;
+        int sum = arr[0];
+        int maxLen = 0;
+        int n = arr.length;
 
-    while (right < n) {
+        while (right < n) {
 
-        // Shrink window if sum becomes greater than k
-        while (left <= right && sum > k) {
-            sum -= arr[left];
-            left++;
+            // Shrink window if sum becomes greater than k
+            while (left <= right && sum > k) {
+                sum -= arr[left];
+                left++;
+            }
+
+            // Check if current window has sum = k
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Expand window
+            right++;
+            if (right < n) {
+                sum += arr[right];
+            }
         }
 
-        // Check if current window has sum = k
-        if (sum == k) {
-            maxLen = Math.max(maxLen, right - left + 1);
-        }
-
-        // Expand window
-        right++;
-        if (right < n) {
-            sum += arr[right];
-        }
+        return maxLen;
     }
-
-    return maxLen;
 }
