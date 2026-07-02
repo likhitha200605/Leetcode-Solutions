6846e94b690f7df69ecb6036e31885b1e08dda19
Likhitha K <likhithak757@gmail.com>
2026-07-02 21:28:51 +0530

    added best time to buy and sell stocks

---
+++ Arrays/Best_time_to_buy_and_sell_stocks_121.java
@@ -0,0 +1,12 @@
+public class Best_time_to_buy_and_sell_stocks_121 {
+    public int maxProfit(int[] prices) {
+        int minp = prices[0];
+        int maxp = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minp;
+            maxp = Math.max(maxp, profit);
+            minp = Math.min(minp, prices[i]);
+        }
+        return maxp;
+    }
+}
