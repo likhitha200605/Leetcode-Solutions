d451884bd6946e20f1820519801a8b2abfb95891
Likhitha K <likhithak757@gmail.com>
2026-07-28 21:37:24 +0530

    pascal_triangle is added

---
+++ Arrays/Pascal_triangle.java
@@ -0,0 +1,24 @@
+class Pascal_triangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> temp = generateRow(i);
+            ans.add(temp);
+        }
+        return ans;
+
+    }
+
+    public List<Integer> generateRow(int row) {
+        long ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.add((int) ans);
+        }
+        return ansRow;
+    }
+
+}
