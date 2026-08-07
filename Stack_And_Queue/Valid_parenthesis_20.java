7cc5eb273df8b8b61712c0f1d997deb8f3c49b46
Likhitha K <likhithak757@gmail.com>
2026-08-07 13:08:50 +0530

    added valid paranthesis

---
+++ Stack_And_Queue/Valid_parenthesis_20.java
@@ -0,0 +1,22 @@
+package Stack_And_Queue;
+
+import java.util.Stack;
+
+public class Valid_parenthesis_20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '[')
+                stack.push(']');
+            else if (c == '{')
+                stack.push('}');
+            else {
+                if (stack.isEmpty() || stack.pop() != c)
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
