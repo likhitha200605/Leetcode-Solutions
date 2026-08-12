94278002b21059a89e3c0ca68c89890af34f08e4
Likhitha K <likhithak757@gmail.com>
2026-08-12 17:52:07 +0530

    added remove k digits

---
+++ Stack_And_Queue/Remove_k_digits_402.java
@@ -0,0 +1,26 @@
+class Remove_k_digits_402 {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+        for (char ch : num.toCharArray()) {
+            while (!st.isEmpty() && k > 0 && st.peek() > ch) {
+                st.pop();
+                k--;
+            }
+            st.push(ch);
+        }
+        while (k > 0) {
+            st.pop();
+            k--;
+        }
+        StringBuilder res = new StringBuilder();
+        while (!st.isEmpty()) {
+            res.append(st.pop());
+        }
+        String result = res.reverse().toString();
+        result = result.replaceFirst("^0+", "");
+        if (result.length() == 0) {
+            return "0";
+        }
+        return result;
+    }
+}
